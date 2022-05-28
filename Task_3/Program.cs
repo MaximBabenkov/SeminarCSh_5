ea99406672b0e988fb58b56502245d8f32a96508
MaximBabenkov <lostbass@mail.ru>
2022-05-28 23:14:03 +0300

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать числа Фибоначчи меньше заданного числа N
+Console.Write("Enter your number N: ");
+int n = int.Parse(Console.ReadLine()??"0");
+int fib1=0;
+int fib2=1;
+int fibSum=0;
+Console.Write($"{fib1} {fib2} ");
+while (fib1+fib2<n)
+{
+    fibSum=fib1+fib2;
+    fib1=fib2;
+    fib2=fibSum;
+    Console.Write($"{fibSum} ");
+}
