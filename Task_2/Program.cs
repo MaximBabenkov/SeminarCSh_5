8b7224f53856663548af7a4230a90e9f48551df9
MaximBabenkov <lostbass@mail.ru>
2022-05-28 23:10:29 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Преобразовать десятичное число в двоичное
+Console.Write("Enter your decimal number: ");
+int a = int.Parse(Console.ReadLine()??"0");
+int b=0;
+string result=" ";
+while (a>0)
+{
+    b=a%2;
+    result=b+result;
+    a=a/2;
+}
+Console.Write($"Your binary number: {result}");
+
